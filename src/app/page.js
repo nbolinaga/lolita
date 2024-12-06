38e91303045d74363a23de4cd6ee5d7d4a6fc401
nbolinaga <nbolinaga17@gmail.com>
2024-12-06 00:06:44 -0400

    added useHooks for setting tab icon and title

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
+'use client'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Banner from "@/components/Banner";
 import OurProducts from "@/components/OurProducts";
 import RequestButton from "@/components/RequestButton";
+import { useDocumentTitle } from "@uidotdev/usehooks";
+import { useFavicon } from "@uidotdev/usehooks";
+import { useState } from "react";
 
 export default function Home() {
+  useDocumentTitle("Lolita | Home");
+  const [favicon] = useState(
+    "/lolitaCircle.png"
+  );
+
+  useFavicon(favicon);
   return (
     <div className="bg-[#fdf8f2] min-h-screen py-6 px-32  font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
       <Navbar />
