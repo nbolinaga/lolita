84b2abc446ca8e6921cbb865d5a91408e4f3e81c
nbolinaga <nbolinaga17@gmail.com>
2024-12-07 10:34:49 -0400

    fixed links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='mt-24 py-6 border-t-gray-400 border-t md:grid grid-cols-3 space-y-12 md:space-y-0 mb-20 md:mb-0'>
+    <div className='relative mt-24 py-6 border-t-gray-400 border-t md:grid grid-cols-3 space-y-12 md:space-y-0 mb-20 md:mb-0 z-50'>
       <p className='col-span-1 flex justify-center items-center text-center'>Lolitadc @ 2024. All Rights Reserved</p>
 
       <div className='col-span-1 flex justify-center items-center text-center'>
@@ -16,7 +16,7 @@ const Footer = () => {
         />
       </div>
       <div className='md:col-span-1 md:flex justify-center items-center w-full'>
-        <a href='https://forms.gle/9GzM4RFztV7ozyT17' target='_blank'><h5 className='md:px-6 text-lg text-slate-800 md:mx-auto w-full text-center'>Contact Us</h5></a>
+        <a href='https://forms.gle/9GzM4RFztV7ozyT17' target='_blank' className=''><h5 className='md:px-6 text-lg text-slate-800 md:mx-auto w-full text-center'>Contact Us</h5></a>
         <a><h5 className='hidden md:block px-6 text-lg text-slate-800 md:mx-auto w-full'>|</h5></a>
         <a href='https://www.instagram.com/_lolitadc' target='_blank'><h5 className='md:px-6 text-lg text-slate-800 mx-auto w-full text-center'>Follow Us</h5></a>
       </div>
